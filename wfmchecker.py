85ede6410b3964b9a9bb8ef6162c8c303d877df9
quirkyturtle9001 <60527681+quirkyturtle9001@users.noreply.github.com>
2025-01-24 19:50:08 +1300

    Update wfmchecker.py
    added better scheduling management and fixed browser error

--- wfmchecker.py
+++ wfmchecker.py
@@ -10,6 +10,8 @@ from selenium.webdriver import Chrome
 import time
 import winsound
 import requests
+import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 # Constants
 #SELLER_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div[1]/div[4]/div"
@@ -19,7 +21,7 @@ BUY_BUTTON_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div/div
 #PRICE_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div[1]/div[4]/div/b"
 PRICE_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div/div[1]/div/div[4]/div/div"
 # Ask the user for the desired pause time between scrapes
-pause_time_input = input("Enter the desired pause time between scrapes (in seconds, don't spam): ")
+pause_time_input = input("Enter the desired pause time between scrapes (in seconds): ")
 pause_time = int(pause_time_input)  # Convert the input to an integer
 
 
@@ -29,13 +31,13 @@ class WarframeMarketScraper:
             self.browserProfile = webdriver.ChromeOptions()
             self.browserProfile.add_experimental_option("excludeSwitches", ['enable-logging'])
             self.browserProfile.add_experimental_option("prefs", {"intl.accept_languages" : "en, en_US" })
-            self.browserProfile.add_argument("--incognito")
+            self.browserProfile.add_argument("--incognito --headless")                     
             # Use ChromeDriverManager to download and install ChromeDriver
             self.browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()) , options=self.browserProfile)
 
         def priceCheck(self):
             with open ("shopping_list.txt", "r", encoding="UTF-8") as file:
-                for item in file.readlines():                    
+                for item in file.readlines():
                     # Construct URL for item on Warframe Market and navigate to it
                     item_url = "https://warframe.market/items/"+ ''.join(c for c in item if not c.isnumeric()).lower().rstrip().replace(' ','_')
                     self.browser.get(item_url)
@@ -50,27 +52,36 @@ class WarframeMarketScraper:
 
                     if price <= target_price:
                         # If price is less than or equal to target price, print success message and copy order message to clipboard
-                        print(f"✔️ '{item.title().rstrip()}' costs {price} platinum, which is less than the amount you are willing to pay. ✔️")
+                        print(f"{datetime.datetime.now()} ✔️ '{item.title().rstrip()}' costs {price} platinum, which is less than the amount you are willing to pay. ✔️")
                         # Play a sound to indicate that the script found a cheaper price
                         winsound.Beep(2500, 500)  # Replace with your preferred sound and duration
                         winsound.Beep(3500, 500)
 
-                        _buybutton_clickability = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, BUY_BUTTON_XPATH))) 
+                        _buybutton_clickability = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, BUY_BUTTON_XPATH)))
                         buy_button = self.browser.find_element("xpath", BUY_BUTTON_XPATH)
-                        buy_button.click()     
+                        buy_button.click()
 
                         copy = webdriver.ActionChains(self.browser)
-                        copy.key_down(Keys.CONTROL).send_keys('C').key_up(Keys.CONTROL).perform()         
+                        copy.key_down(Keys.CONTROL).send_keys('C').key_up(Keys.CONTROL).perform()
 
                     else:
                         # If price is more than target price, print a message indicating that the item is too expensive         
-                        print(f"❌ {price} platinum for '{''.join(c for c in item if not c.isnumeric()).rstrip()}' is more than the {item.split()[-1]} platinum you are willing to pay. ❌")
+                        print(f"{datetime.datetime.now()} ❌ {price} platinum for '{''.join(c for c in item if not c.isnumeric()).rstrip()}' is more than the {item.split()[-1]} platinum you are willing to pay. ❌")
                 
-            self.browser.quit()
+            # self.browser.quit()
 
         
             
 wfm = WarframeMarketScraper()
-while True: 
-    wfm.priceCheck()
-    time.sleep(pause_time) # pause till it scrapes again
+"""try:
+    while True: 
+        wfm.priceCheck()
+        time.sleep(pause_time) # pause till it scrapes again
+except KeyboardInterrupt:
+    wfm.browser.quit()"""
+scheduler = BlockingScheduler()
+job = scheduler.add_job(wfm.priceCheck, 'interval', seconds=pause_time)
+try:
+    scheduler.start()
+except KeyboardInterrupt:
+    scheduler.shutdown()
