06d9143664b1ab654ed263659314c236e58ab335
Turtlesson <60527681+Turtlesson@users.noreply.github.com>
2025-01-25 09:50:19 +1300

    Update wfmchecker.py
    added ability to exit script and comment out shopping list items with #

--- wfmchecker.py
+++ wfmchecker.py
@@ -1,3 +1,11 @@
+import threading
+import time
+import winsound
+import requests
+import datetime
+import signal
+import sys
+import keyboard
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,19 +15,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import Chrome
-import time
-import winsound
-import requests
-import datetime
+from selenium.common.exceptions import TimeoutException
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 # Constants
-#SELLER_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div[1]/div[4]/div"
 SELLER_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div/div[1]/div/div[1]"
-#BUY_BUTTON_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div[1]/div[6]/button"
-BUY_BUTTON_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div/div[1]/div/div[7]/button"
-#PRICE_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div[1]/div[4]/div/b"
+BUY_BUTTON_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div/div[1]/div/div[6]/button"
 PRICE_XPATH = "//*[@id='panel']/section[2]/div[3]/div[2]/div[2]/div/div/div[1]/div/div[4]/div/div"
+
 # Ask the user for the desired pause time between scrapes
 pause_time_input = input("Enter the desired pause time between scrapes (in seconds): ")
 pause_time = int(pause_time_input)  # Convert the input to an integer
@@ -31,57 +34,70 @@ class WarframeMarketScraper:
             self.browserProfile = webdriver.ChromeOptions()
             self.browserProfile.add_experimental_option("excludeSwitches", ['enable-logging'])
             self.browserProfile.add_experimental_option("prefs", {"intl.accept_languages" : "en, en_US" })
-            self.browserProfile.add_argument("--incognito --headless")                     
+            self.browserProfile.add_argument("--incognito --headless")
             # Use ChromeDriverManager to download and install ChromeDriver
             self.browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()) , options=self.browserProfile)
-
+        
         def priceCheck(self):
             with open ("shopping_list.txt", "r", encoding="UTF-8") as file:
                 for item in file.readlines():
-                    # Construct URL for item on Warframe Market and navigate to it
-                    item_url = "https://warframe.market/items/"+ ''.join(c for c in item if not c.isnumeric()).lower().rstrip().replace(' ','_')
-                    self.browser.get(item_url)
-                    
-                    # Wait for seller visibility and retrieve price element
-                    _seller_visibility = WebDriverWait(self.browser, 10).until(EC.visibility_of_all_elements_located((By.XPATH, SELLER_XPATH)))
-                    price_element = self.browser.find_element("xpath", PRICE_XPATH)
+                    if not item.lstrip().startswith("#"):
+                        # Construct URL for item on Warframe Market and navigate to it
+                        item_url = "https://warframe.market/items/" + ''.join(c for c in item if not c.isnumeric()).lower().rstrip().replace(' ', '_')
+                        self.browser.get(item_url)
 
-                    # Convert price and target price to integers and compare
-                    price = int(price_element.text)
-                    target_price = int(item.split()[-1])
+                        # Wait for seller visibility and retrieve price element, hopefully catching exceptions if thrown
+                        try:
+                            _seller_visibility = WebDriverWait(self.browser, 10).until(EC.visibility_of_all_elements_located((By.XPATH, SELLER_XPATH)))
+                            price_element = self.browser.find_element("xpath", PRICE_XPATH)
+                        except TimeoutException as e:
+                            print(f"TimeoutError: {e}")
+                            continue
 
-                    if price <= target_price:
-                        # If price is less than or equal to target price, print success message and copy order message to clipboard
-                        print(f"{datetime.datetime.now()} ✔️ '{item.title().rstrip()}' costs {price} platinum, which is less than the amount you are willing to pay. ✔️")
-                        # Play a sound to indicate that the script found a cheaper price
-                        winsound.Beep(2500, 500)  # Replace with your preferred sound and duration
-                        winsound.Beep(3500, 500)
+                        # Convert price and target price to integers and compare
+                        price = int(price_element.text)
+                        target_price = int(item.split()[-1])
 
-                        _buybutton_clickability = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, BUY_BUTTON_XPATH)))
-                        buy_button = self.browser.find_element("xpath", BUY_BUTTON_XPATH)
-                        buy_button.click()
+                        if price <= target_price:
+                            # If price is less than or equal to target price, print success message and copy order message to clipboard
+                            print(f"{datetime.datetime.now()} ✔️ '{item.title().rstrip()}' costs {price} platinum, which is less than the amount you are willing to pay. ✔️")
+                            # Play a sound to indicate that the script found a cheaper price
+                            winsound.Beep(2500, 1000)  # Replace with your preferred sound and duration
+                            winsound.Beep(3500, 1000)
 
-                        copy = webdriver.ActionChains(self.browser)
-                        copy.key_down(Keys.CONTROL).send_keys('C').key_up(Keys.CONTROL).perform()
+                            _buybutton_clickability = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, BUY_BUTTON_XPATH)))
+                            buy_button = self.browser.find_element("xpath", BUY_BUTTON_XPATH)
+                            buy_button.click()
 
-                    else:
-                        # If price is more than target price, print a message indicating that the item is too expensive         
-                        print(f"{datetime.datetime.now()} ❌ {price} platinum for '{''.join(c for c in item if not c.isnumeric()).rstrip()}' is more than the {item.split()[-1]} platinum you are willing to pay. ❌")
-                
-            # self.browser.quit()
+                            copy = webdriver.ActionChains(self.browser)
+                            copy.key_down(Keys.CONTROL).send_keys('C').key_up(Keys.CONTROL).perform()
 
-        
-            
-wfm = WarframeMarketScraper()
-"""try:
-    while True: 
-        wfm.priceCheck()
-        time.sleep(pause_time) # pause till it scrapes again
-except KeyboardInterrupt:
-    wfm.browser.quit()"""
-scheduler = BlockingScheduler()
-job = scheduler.add_job(wfm.priceCheck, 'interval', seconds=pause_time)
-try:
-    scheduler.start()
-except KeyboardInterrupt:
-    scheduler.shutdown()
+                        else:
+                            # If price is more than target price, print a message indicating that the item is too expensive         
+                            print(f"{datetime.datetime.now()} ❌ {price} platinum for '{''.join(c for c in item if not c.isnumeric()).rstrip()}' is more than the {item.split()[-1]} platinum you are willing to pay. ❌")       
+
+def main():
+    wfm = WarframeMarketScraper()
+    scheduler = BlockingScheduler()
+
+    # Do the initial scrape
+    wfm.priceCheck()
+
+    job = scheduler.add_job(wfm.priceCheck, 'interval', seconds=pause_time)
+
+    def sigterm_handler(signal, frame):
+        print("Script interrupted. Shutting down scheduler and exiting.")
+        scheduler.shutdown()
+        sys.exit(0)
+
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    try:
+        scheduler.start()
+    except KeyboardInterrupt:
+        print("Script interrupted. Shutting down scheduler and exiting.")
+        scheduler.shutdown()
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
